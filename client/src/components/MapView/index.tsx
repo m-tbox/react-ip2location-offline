d38395e5f0d5730fa459e029fd06053e2bda0da5
Locaided <locaided@example.com>
2022-05-26 00:43:01 +0500

    Parsed CSV to read geo ip data

--- client/src/components/MapView/index.tsx
+++ client/src/components/MapView/index.tsx
@@ -1,93 +1,130 @@
 import { useEffect, useState } from 'react'
+import Papa from 'papaparse'
+import { VectorMap } from "@react-jvectormap/core"
+import { worldMill } from "@react-jvectormap/world"
+
 import './styles.css'
-import axios from 'axios'
-
-import { VectorMap } from "@react-jvectormap/core";
-import { worldMill } from "@react-jvectormap/world";
-
-
-interface ipData {
-    range: number[];
-    country: string;
-    region: string;
-    eu: string;
-    timezone: string;
-    city: string;
-    ll: number[];
-    metro: number;
-    area: number;
-}
-
-const BASE_URL = 'http://localhost:8080/location'
 
 const MapView = () => {
-    const [locData, setLocData] = useState<any>([])
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState<boolean>();
+    const [locData, setLocData] = useState()
+    const [markers, setMarkers] = useState<any>()
 
-    const callLoginApi = async (ip: string) => {
-        const response = await axios.get(`${BASE_URL}/${ip}`, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            }
-        });
+    // const fetchCsv = async () => {
+    //     const response = await fetch('/IP2LOCATION-LITE-DB5.csv') as any;
+    //     const reader = response.body.getReader();
+    //     const result = await reader.read();
+    //     const decoder = new TextDecoder('utf-8');
+    //     const csv = await decoder.decode(result.value);
+    //     return csv;
+    // }
 
-        return response.data
-    }
 
-    const readFile = () => {
+    async function getGraphData() {
         setLoading(true)
 
+        let graphData = [] as any
+        graphData = await readAndParseData()
+
+        console.log(graphData, 'ggg')
         fetch('/ip.txt')
-            .then((r: any) => r.text())
-            .then(async (textData: any) => {
+            .then((r) => r.text())
+            .then(textData => {
 
                 let ipAddressesFromFile = textData.split('\n')
 
-                // Remove empty ip in array if New line is entered at the end of line
-                ipAddressesFromFile = ipAddressesFromFile.filter((ip: any) => ip != '')
+                if (graphData.length) {
 
-                let dataArr: ipData[] = await Promise.all(ipAddressesFromFile.map(async (ip: string) => {
-                    if (ip) {
-                        const response = await callLoginApi(ip);
-                        return response.data;
-                    }
-                }))
+                    let ipNumber = 0
+                    let markersArr: any[] = []
 
-                setLocData(dataArr)
-                setLoading(false)
+                    ipAddressesFromFile.map(ip => {
+                        const splitIp = ip.split('.')
+
+                        const w = splitIp?.[0] as any
+                        const x = splitIp?.[1] as any
+                        const y = splitIp?.[2] as any
+                        const z = 0
+
+                        ipNumber = (16777216 * w) + (65536 * x) + (256 * y) + z
+
+
+                        let index = graphData.findIndex(function (el: any) {
+                            return el[0] == ipNumber
+                        });
+
+                        if (index > -1 && graphData[index]) {
+                            markersArr.push([
+                                graphData[index][6],
+                                graphData[index][7]
+                            ])
+                            console.log('Data found at index', index)
+                            console.log('Data', graphData[index])
+                        }
+                    })
+
+                    setMarkers(markersArr)
+                }
             })
+
+        setLoading(false)
+
+        console.log('length', markers)
+        setLocData(graphData)
+    }
+
+    const readAndParseData = async () => {
+        const response = await fetch('/IP2LOCATION-LITE-DB5.csv') as any
+        const reader = response.body.getReader()
+        const decoder = new TextDecoder('utf-8')
+
+        let csvText = ""
+        let value, done;
+        while (!done) {
+            ({ value, done } = await reader.read());
+            if (done) {
+                break
+            }
+
+            const csv = decoder.decode(value)
+            csvText = csvText.concat(csv)
+        }
+
+        const results = await Papa.parse(csvText, { header: false })
+        const rows = results.data
+
+        return rows
     }
 
     useEffect(() => {
-        readFile()
+        getGraphData()
     }, [])
 
 
     // Format data according to map marker requirements
-    let markers = locData.map((data: any, idx: number) => {
+    // let markers = locData.map((data: any, idx: number) => {
 
-        if (data?.ll && data?.ll.length) {
-            let obj = {
-                latLng: data?.ll,
-                name: `lat: ${data?.ll[0]}, lng: ${data?.ll[1]}, ${data?.country}`
+    //     if (data?.ll && data?.ll.length) {
+    //         let obj = {
+    //             latLng: data?.ll,
+    //             name: `lat: ${data?.ll[0]}, lng: ${data?.ll[1]}, ${data?.country}`
 
-            }
+    //         }
 
-            return obj
-        } else {
-            return []
-        }
-    })
+    //         return obj
+    //     } else {
+    //         return []
+    //     }
+    // })
 
-    // console.log(markers, 'marker', locData)
+    // console.log('marker', locData)
 
     return (
         <>
             {
                 loading ? <span> ... Loading</span> :
                     <>
-                        {
+                        {/* {
                             locData && locData.length > 0 &&
                             <VectorMap
                                 map={worldMill}
@@ -96,7 +133,7 @@ const MapView = () => {
                                     e.preventDefault();
                                 }}
                             />
-                        }
+                        } */}
                     </>
             }
         </>
